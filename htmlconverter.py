1671cb6f48bcc4efef213fd27cbb30797d446994
devensiv <devensiv@gmail.com>
2021-04-16 15:56:47 +0200

    added converter and updated gitignore to ignore html and new files

---
+++ htmlconverter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+
+if len(sys.argv) != 2:
+    print ("please supply a file as parameter")
+    exit(1)
+
+path = sys.argv[1]
+with open(path) as rfile:
+    with open(path + ".new", "w") as wfile:
+        for line in rfile:
+            if (line == "\n"):
+                continue
+            wfile.write("\"" + line[:-1] + "\",\n")
+            print("\"" + line[:-1] + "\",")
+        wfile.write("\"\"")
+        print("\"\"")
